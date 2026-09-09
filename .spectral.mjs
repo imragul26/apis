e8620c9f315055eb4f2aabcc957eff0b1454ce91
imragul26@gmail.com <imragul26@gmail.com>
2026-09-10 00:24:04 +0530

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMyMDM3Ng/spectral.js?branch_slug=main&token=a1dc81d8-b978-4609-b88c-539672a53ee6";
+export default { extends: ruleset };
